fab7caa751d776bfcea224fcb886c4484c3152e1
Ashe Connor <ashe@kivikakk.ee>
2019-02-19 11:52:51 +1100

    preserve symlinks

--- ProjFS.Linux/Scripts/Build.sh
+++ ProjFS.Linux/Scripts/Build.sh
@@ -18,7 +18,7 @@ PROJFS=$SRCDIR/ProjFS.Linux
 
 (cd $PROJFS/libprojfs-vfsapi && ./autogen.sh && ./configure && make)
 mkdir -p $ROOTDIR/BuildOutput/ProjFS.Linux/Native/$CONFIGURATION
-cp $PROJFS/libprojfs-vfsapi/lib/.libs/libprojfs-vfsapi.so* $ROOTDIR/BuildOutput/ProjFS.Linux/Native/$CONFIGURATION
+cp -P $PROJFS/libprojfs-vfsapi/lib/.libs/libprojfs-vfsapi.so* $ROOTDIR/BuildOutput/ProjFS.Linux/Native/$CONFIGURATION
 
 dotnet restore $PROJFS/PrjFSLib.Linux.Managed/PrjFSLib.Linux.Managed.csproj /p:Configuration=$CONFIGURATION /p:Platform=x64 --packages $PACKAGES || exit 1
 dotnet build $PROJFS/PrjFSLib.Linux.Managed/PrjFSLib.Linux.Managed.csproj /p:Configuration=$CONFIGURATION /p:Platform=x64 || exit 1
